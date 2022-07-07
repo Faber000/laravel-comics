96392ff0eb8a916f07b05dba988e26894b04d0fe
Fabio Moro <fabio.moro2000@gmail.com>
2022-07-07 14:50:20 +0200

    aggiunta header

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.base')
+
+@section('page-title')
+    Homepage
+@endsection
+
+@section('page-content')
+    <section>
+        
+    </section>
+@endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,32 @@
+<header>
+    <div class="header-top">
+        <div class="container flex-end">
+            <div>DC POWER VISA</div>
+            <div>ADDITIONAL DC SITES</div>
+        </div>
+    </div>
+    <div class="header-middle flex">
+        <div class="container">
+            <nav class="main-nav">
+                <div class="logo-div">
+                    <img class="logo" src="{{asset('img/dc-logo.png')}}"/>
+                </div>
+                <ul>
+                    <li>CHARACTERS</li>
+                    <li>COMICS</li>
+                    <li>MOVIES</li>
+                    <li>TV</li>
+                    <li>GAMES</li>
+                    <li>COLLECTIBLES</li>
+                    <li>VIDEOS</li>
+                    <li>FANS</li>
+                    <li>NEWS</li>
+                    <li>SHOP</li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+    <div class="header-bottom">
+        <img src="{{asset('img/jumbotron.jpg')}}" alt="">
+    </div>
+</header>
